74c3e221e8a6d39062d87e7b2c2f9520ef75afcf
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 13:15:53 -0300

    Ejercicio V
    Ejercicio V - Ejercicio Repaso

---
+++ CAI_EjercicioV/CAI_EjercicioV/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número: ");
+            string numero = Console.ReadLine();
+            Console.Write("El número invertido es: ");
+            for (int i = numero.Length-1; i >= 0; i--)
+            {
+                Console.Write(numero[i]);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
